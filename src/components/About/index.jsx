f3cdcaa0e13c98fe9291e3fcb2deb26f4cae1893
Peter Gwaka <peter@deeplearning-cafe.com>
2023-11-04 16:23:50 +0200

    update conventions

---
+++ src/components/About/index.jsx
@@ -0,0 +1,11 @@
+import './styles';
+
+function About() {
+    return (
+      <div>
+        About
+      </div>
+    );
+  }
+  
+  export default About;
---
+++ src/components/ClientGallery/index.jsx
@@ -0,0 +1,11 @@
+import './styles';
+
+function ClientGallery() {
+    return (
+      <div>
+        Client Gallery
+      </div>
+    );
+  }
+  
+  export default ClientGallery;
